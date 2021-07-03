35b37096dc46ca2d608d18f46e87dc8a0a9e516d
Manish Sah <48799735+csemanish12@users.noreply.github.com>
2021-07-03 10:33:29 +0545

    add implementation of lambda

---
+++ functions/lambda.py
@@ -0,0 +1,5 @@
+# Python lambdas are little, anonymous functions, subject to a more restrictive but more concise syntax
+# than regular Python functions.
+
+result = (lambda x, y, z: x + y + z)
+print('print result:', result(1, 2, 3))
