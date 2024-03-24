f58a3d649f752eb440625a2c656c50cc9fe32471
Manish <sahmanish20@gmail.com>
2024-03-24 09:32:16 +0545

    refactor queue implementation

--- Data_structures/queue_using_list.py
+++ Data_structures/queue_using_list.py
@@ -17,31 +17,38 @@ class QueueViaList:
             print("Queue is full")
             return
 
-        if self.rear >= self.capacity:
-            empty_index = self.rear % self.capacity
-            print("empty index is now:", empty_index)
-            self.queue[empty_index] = element
-        else:
-            self.queue[self.rear] = element
-
+        empty_index = self.rear % self.capacity
+        self.queue[empty_index] = element
         self.rear += 1
         self.count += 1
+
+        self.debug_queue()
 
     def pop(self):
         if self.count == 0:
             print("Queue is empty")
             return
 
-        element = self.queue[self.front]
-        self.queue[self.front] = None
+        front_index = self.front % self.capacity
+        element = self.queue[front_index]
+        self.queue[front_index] = None
         self.count -= 1
-        if self.front == self.capacity - 1:
-            self.front = 0
-        else:
-            self.front += 1
+        self.front += 1
         print("popped element is:", element)
+        self.debug_queue()
         return element
 
     def debug_queue(self):
         print("queue:", self.queue, " rear is:", self.rear, " front is:", self.front)
 
+
+q = QueueViaList()
+q.push(1)
+q.push(2)
+q.push(3)
+q.push(4)
+q.push(5)
+q.push(6)
+q.pop()
+q.pop()
+q.push(7)
