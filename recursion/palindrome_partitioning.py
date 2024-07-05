04ebde501262033c91e64ffd25e71f3f0eaec48c
Manish <sahmanish20@gmail.com>
2024-07-05 08:08:14 +0545

    update time and space complexity for palindrome partitioning

--- recursion/palindrome_partitioning.py
+++ recursion/palindrome_partitioning.py
@@ -32,6 +32,16 @@ from typing import List
 class Solution:
     """
     Partition at each index recursively
+
+    Time Complexity: O( (2^n) *k*(n/2) )
+    Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome.
+    O(k) is for inserting the palindromes in another data structure, where k  is the average length of the
+    palindrome list.
+
+    Space Complexity: O(k * x)
+    Reason: The space complexity can vary depending on the length of the answer. k is the average length of the list
+    of palindromes and if we have x such list of palindromes in our final answer. The depth of the recursion tree
+    is n, so the auxiliary space required is equal to the O(n).
     """
     def partition(self, s: str) -> List[List[str]]:
         ans = []
