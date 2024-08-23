06a1297be9b304af4bb3fed824c6a100077955dd
Manish <sahmanish20@gmail.com>
2024-08-23 07:48:29 +0545

    add time complexity

--- tree/inorder_traversal.py
+++ tree/inorder_traversal.py
@@ -25,6 +25,17 @@ The number of nodes in the tree is in the range [0, 100].
 
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
+
+Time Complexity: O(N) where N is the number of nodes in the binary tree as each node of the
+binary tree is visited exactly once.
+
+Space Complexity: O(N) where N is the number of nodes in the binary tree. This is because the
+recursive stack uses an auxiliary space which can potentially hold all nodes in the tree when
+dealing with a skewed tree (all nodes have only one child), consuming space proportional to
+the number of nodes.In the average case or for a balanced tree, the maximum number of nodes
+that could be in the stack at any given time would be roughly the height of the tree hence O(log2N).
+
+
 """
 from typing import Optional, List
 
